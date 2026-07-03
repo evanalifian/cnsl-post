d93be2f90b54996d1507097e4e7faed10dfbf1e7
evanalifian <evanalifian@gmail.com>
2026-07-03 18:26:28 +0700

    fix(search): change search query mode to BOOLEAN for improved user lookup

--- src/Repository/SearchRepository.php
+++ src/Repository/SearchRepository.php
@@ -16,9 +16,9 @@ class SearchRepository
   public function findUsers(string $query): array
   {
     try {
-      $statement = self::$connDB->prepare("SELECT * FROM users WHERE MATCH (username, display_name) AGAINST (? IN NATURAL LANGUAGE MODE)");
+      $statement = self::$connDB->prepare("SELECT * FROM users WHERE MATCH (username, display_name) AGAINST (? IN BOOLEAN MODE)");
       $statement->execute([$query]);
-      return $statement->fetchAll()?:[];
+      return $statement->fetchAll() ?: [];
     } catch (\Exception $e) {
       throw new ValidationException($e->getMessage());
     }
